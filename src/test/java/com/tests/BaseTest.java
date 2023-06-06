39f6a382b88e5d3c3de9e46b2b23ed459f4fd9fc
DeepaRajndr <133270634+DeepaRajndr@users.noreply.github.com>
2023-06-06 09:31:44 -0400

    Modified Base Class

--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -1,13 +1,11 @@
 package com.tests;
 
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import java.net.MalformedURLException;
@@ -23,35 +21,23 @@ public class BaseTest {
         // HUB_HOST => localhost / 10.0.1.3 / hostname
 
         String host = "localhost";
-        //DesiredCapabilities dc;
-        ChromeOptions chromeOptions = new ChromeOptions();
-        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        MutableCapabilities dc;
 
+        if (System.getProperty("BROWSER") != null &&
+                System.getProperty("BROWSER").equalsIgnoreCase("firefox")) {
+            dc = new FirefoxOptions();
+        } else {
+            dc = new ChromeOptions();
+        }
 
-        if(System.getProperty("HUB_HOST") != null){
+        if (System.getProperty("HUB_HOST") != null) {
             host = System.getProperty("HUB_HOST");
         }
 
         String testName = ctx.getCurrentXmlTest().getName();
 
         String completeUrl = "http://" + host + ":4444/wd/hub";
-
-        if(System.getProperty("BROWSER") != null &&
-            System.getProperty("BROWSER").equalsIgnoreCase("firefox")){
-            firefoxOptions.setCapability("name", testName);
-            driver = new RemoteWebDriver(new URL(completeUrl),firefoxOptions);
-
-        }else{
-            chromeOptions.setCapability("name",testName);
-            driver = new RemoteWebDriver(new URL(completeUrl),chromeOptions);
-        }
+        dc.setCapability("name", testName);
+        this.driver = new RemoteWebDriver(new URL(completeUrl), dc);
     }
-
-    @AfterTest
-    public void quitDriver(){
-        this.driver.quit();
-    }
-
-
-
 }
